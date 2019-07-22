37eac4565ed6725993a073dd511091affd97345b
grom194 <brskoshelev@gmail.com>
2019-07-22 20:07:18 +0300

    task_4: move task data to fixtures directory

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,16 +1,12 @@
+import { readFileSync } from 'fs';
 import gendiff from '../src/gendiff';
 
 test('test json compare', () => {
-  const pathToFile1 = './__tests__/testPack1/before.json';
-  const pathToFile2 = './__tests__/testPack1/after.json';
-
-  const result = `{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
-  expect(gendiff(pathToFile1, pathToFile2)).toBe(result);
+  const pathToFile1 = './__tests__/__fixtures__/testPack1/before.json';
+  const pathToFile2 = './__tests__/__fixtures__/testPack1/after.json';
+  const output = readFileSync(
+    './__tests__/__fixtures__/testPack1/output.txt',
+    'utf8',
+  );
+  expect(gendiff(pathToFile1, pathToFile2)).toBe(output);
 });
