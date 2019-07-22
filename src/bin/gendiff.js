5a6460cea4c85cabaad7ec27df8e918457134411
grom194 <brskoshelev@gmail.com>
2019-07-22 14:07:36 +0300

    task_2: add format option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
+
 import program from 'commander';
 import packageData from '../../package.json';
 
 program
+  .arguments('<firstConfig>')
+  .arguments('<secondConfig>')
   .description(packageData.description)
   .version(packageData.version)
+  .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => {})
   .parse(process.argv);
