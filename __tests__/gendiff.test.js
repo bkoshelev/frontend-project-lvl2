861da15f48438725c80740f298a2fb94a9a8c617
grom194 <brskoshelev@gmail.com>
2019-07-22 20:17:53 +0300

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,11 +1,13 @@
 import { readFileSync } from 'fs';
+import path from 'path';
 import gendiff from '../src/gendiff';
 
 test('test json compare', () => {
-  const pathToFile1 = './__tests__/__fixtures__/testPack1/before.json';
-  const pathToFile2 = './__tests__/__fixtures__/testPack1/after.json';
+  const pathToFixtures = '/__fixtures__/testPack1/';
+  const pathToFile1 = path.join(__dirname, pathToFixtures, 'before.json');
+  const pathToFile2 = path.join(__dirname, pathToFixtures, 'after.json');
   const output = readFileSync(
-    './__tests__/__fixtures__/testPack1/output.txt',
+    path.join(__dirname, pathToFixtures, 'output.txt'),
     'utf8',
   );
   expect(gendiff(pathToFile1, pathToFile2)).toBe(output);
