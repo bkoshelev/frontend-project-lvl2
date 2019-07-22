201fdfa6333dc7472e6f50035d895c76bdc072d7
grom194 <brskoshelev@gmail.com>
2019-07-22 20:08:02 +0300

    refactor genDiff (pipeline structure)

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,42 +2,50 @@ import { readFileSync } from 'fs';
 import { has } from 'lodash/fp';
 import { resolve } from 'path';
 
-const gendiff = (pathToFile1, pathToFile2) => {
-  const file1Content = readFileSync(resolve(pathToFile1));
-  const file2Content = readFileSync(resolve(pathToFile2));
+const readFiles = ([pathToFile1, pathToFile2]) => [
+  readFileSync(resolve(pathToFile1)),
+  readFileSync(resolve(pathToFile2)),
+];
 
-  const beforeJson = JSON.parse(file1Content);
-  const afterJson = JSON.parse(file2Content);
+const parseFilesContent = ([file1Content, file2Content]) => [
+  JSON.parse(file1Content),
+  JSON.parse(file2Content),
+];
 
+const generateDiffOutput = ([beforeJson, afterJson]) => {
   const keys = [...Object.keys(beforeJson), ...Object.keys(afterJson)].reduce(
     (acc, key) => (acc.includes(key) ? acc : [...acc, key]),
     [],
   );
 
   const outputJson = keys.reduce((acc, key) => {
+    const newAcc = [...acc];
     if (has(key, afterJson)) {
       if (has(key, beforeJson)) {
         if (afterJson[key] === beforeJson[key]) {
-          acc[`  ${key}`] = beforeJson[key];
+          newAcc.push(`    ${key}: ${beforeJson[key]}\n`);
         } else {
-          acc[`- ${key}`] = beforeJson[key];
-          acc[`+ ${key}`] = afterJson[key];
+          newAcc.push(`  - ${key}: ${beforeJson[key]}\n`);
+          newAcc.push(`  + ${key}: ${afterJson[key]}\n`);
         }
       } else {
-        acc[`+ ${key}`] = afterJson[key];
+        newAcc.push(`  + ${key}: ${afterJson[key]}\n`);
       }
     } else {
-      acc[`- ${key}`] = beforeJson[key];
+      newAcc.push(`  - ${key}: ${beforeJson[key]}\n`);
     }
-    return acc;
-  }, {});
+    return newAcc;
+  }, []);
+  const output = ['{\n', ...outputJson, '}'].join('');
+  return output;
+};
 
-  const outputValuesToString = Object.entries(outputJson)
-    .map(([key, value]) => `  ${key}: ${value}\n`)
-    .join('');
-
-  const outputJsonToString = `{\n${outputValuesToString}}`;
-  return outputJsonToString;
+const gendiff = (pathToFile1, pathToFile2) => {
+  const diffOutput =    [pathToFile1, pathToFile2]
+    |> readFiles
+    |> parseFilesContent
+    |> generateDiffOutput;
+  return diffOutput;
 };
 
 export default gendiff;
