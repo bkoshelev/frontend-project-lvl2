4bc27ac9f9aec5e9a016c34f05085a40b8cf2bd9
grom194 <brskoshelev@gmail.com>
2019-07-22 20:41:08 +0300

    fix eslint prettier conflict: pipleline operator

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,21 +1,21 @@
-import { readFileSync } from 'fs';
-import { has } from 'lodash/fp';
-import { resolve } from 'path';
+import { readFileSync } from "fs";
+import { has } from "lodash/fp";
+import { resolve } from "path";
 
 const readFiles = ([pathToFile1, pathToFile2]) => [
   readFileSync(resolve(pathToFile1)),
-  readFileSync(resolve(pathToFile2)),
+  readFileSync(resolve(pathToFile2))
 ];
 
 const parseFilesContent = ([file1Content, file2Content]) => [
   JSON.parse(file1Content),
-  JSON.parse(file2Content),
+  JSON.parse(file2Content)
 ];
 
 const generateDiffOutput = ([beforeJson, afterJson]) => {
   const keys = [...Object.keys(beforeJson), ...Object.keys(afterJson)].reduce(
     (acc, key) => (acc.includes(key) ? acc : [...acc, key]),
-    [],
+    []
   );
 
   const outputJson = keys.reduce((acc, key) => {
@@ -36,12 +36,13 @@ const generateDiffOutput = ([beforeJson, afterJson]) => {
     }
     return newAcc;
   }, []);
-  const output = ['{\n', ...outputJson, '}'].join('');
+  const output = ["{\n", ...outputJson, "}"].join("");
   return output;
 };
 
 const gendiff = (pathToFile1, pathToFile2) => {
-  const diffOutput =    [pathToFile1, pathToFile2]
+  const diffOutput =
+    [pathToFile1, pathToFile2]
     |> readFiles
     |> parseFilesContent
     |> generateDiffOutput;
