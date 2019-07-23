6a43aa012a457bba8a9b440cf3d26940849f08c2
grom194 <brskoshelev@gmail.com>
2019-07-24 01:26:06 +0300

    task_9: add test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -78,3 +78,16 @@ test("test deep json diff with plain format", () => {
   );
   expect(gendiff(pathToFile1, pathToFile2, "plain")).toBe(output);
 });
+
+test("test deep json diff with json format", () => {
+  const pathToFixtures = "/__fixtures__/testPack8/";
+  const pathToFile1 = path.join(__dirname, pathToFixtures, "before.json");
+  const pathToFile2 = path.join(__dirname, pathToFixtures, "after.json");
+  const output = readFileSync(
+    path.join(__dirname, pathToFixtures, "output.json"),
+    "utf8"
+  );
+  expect(gendiff(pathToFile1, pathToFile2, "json")).toStrictEqual(
+    JSON.parse(output)
+  );
+});
