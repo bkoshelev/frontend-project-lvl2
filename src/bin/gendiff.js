24da3d4d4873eb75cfb5a5c1be480237d0ac05f6
grom194 <brskoshelev@gmail.com>
2019-07-24 00:17:26 +0300

    add argument validation

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
 
-import program from 'commander';
-import packageData from '../../package.json';
+import program from "commander";
+import { existsSync } from "fs";
+import packageData from "../../package.json";
 
-import gendiff from '../gendiff';
+import gendiff from "../gendiff";
 
 program
-  .arguments('<firstConfig>')
-  .arguments('<secondConfig>')
+  .arguments("<firstConfig>")
+  .arguments("<secondConfig>")
   .description(packageData.description)
   .version(packageData.version)
-  .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => {
-    const output = gendiff(firstConfig, secondConfig);
+  .option("-f, --format [type]", "Output format", "json")
+  .action((firstConfig, secondConfig, { format }) => {
+    [firstConfig, secondConfig].forEach(path => {
+      if (!existsSync(path)) {
+        throw Error(`file ${path} not exist`);
+      }
+    });
+    if (!["json", "plain"].includes(format)) {
+      throw Error("invalid format argument");
+    }
+
+    const output = gendiff(firstConfig, secondConfig, format);
     console.log(output);
   })
   .parse(process.argv);
