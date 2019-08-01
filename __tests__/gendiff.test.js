4f0c567f4b12db6f6939543c6433a1739ff8053a
grom194 <brskoshelev@gmail.com>
2019-08-01 20:58:03 +0300

    final_task: refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,52 +1,40 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import gendiff from '../src';
-import { outputFormatOptionPossibleValues } from '../src/formatters';
 
-const { defaultOption, plain, json } = outputFormatOptionPossibleValues;
-describe('nested structure', () => {
-  test.each`
-    testPackNumber | fileExtension |    outputFormat
-        ${1}       |   ${'json'}   |    ${undefined}
-        ${1}       |   ${'json'}   |    ${defaultOption}
-        ${2}       |   ${'yaml'}   |    ${plain}
-        ${3}       |   ${'ini'}    |    ${json}
-  `('test $fileExtension file extension / nested structure / $outputFormat output format',
-  ({ testPackNumber, fileExtension, outputFormat }) => {
-    const pathToFixtures = `/__fixtures__/testPack${testPackNumber}/`;
+/**
+ * Описание
+ *
+ * testName // Название теста
+ * fileExtension // расширение сравниваемых файлов
+ * outputFormat // выбранный тип форматирования результата выполнения
+ * fixtureFolder // название папки с тестовыми данными
+ *
+ * pathToFixtures // путь к тестовым данным текущего теста
+ * pathToFile1 // путь к первому сравниваемому файлу
+ * pathToFile2 // путь ко второму сравниваемому файлу
+ * pathToOutputFile // путь к файлу с резултатом выполнения пакета
+ *
+ */
 
-    const pathToFile1 = path.join(__dirname, pathToFixtures, `file1.${fileExtension}`);
-    const pathToFile2 = path.join(__dirname, pathToFixtures, `file2.${fileExtension}`);
-    const outputFileContent = readFileSync(
-      path.join(__dirname, pathToFixtures, 'output.txt'),
+test.each([
+  ['diff nested structures', 'json', 'json', 'defaultOption', 'nested_structure'],
+  ['nested structure', 'ini', 'ini', 'plain', 'nested_structure'],
+  ['nested structure', 'yaml', 'yaml', 'json', 'nested_structure'],
+  ['nested structure', 'yaml', 'yaml', 'json', 'nested_structure'],
+])('%#. test %s: diff <file1.%s , file2.%s> -f %s ',
+  (testName, file1Extension, file2Extension, outputFormat, fixtureFolder) => {
+    const pathToFixtures = `/__fixtures__/${fixtureFolder}/`;
+    const pathToFile1 = path.join(__dirname, pathToFixtures, 'inputData', file1Extension, `file1.${file1Extension}`);
+    const pathToFile2 = path.join(__dirname, pathToFixtures, 'inputData', file2Extension, `file2.${file2Extension}`);
+    const pathToOutputFile = path.join(__dirname, pathToFixtures, 'outputData', `${outputFormat}.txt`);
+
+    const output = readFileSync(
+      pathToOutputFile,
       'utf8',
     );
-    expect(gendiff(pathToFile1, pathToFile2, outputFormat)).toBe(outputFileContent);
-  });
-});
 
-test('test json file extension / empty file', () => {
-  const pathToFixtures = '/__fixtures__/testPack4/';
-  const pathToFile1 = path.join(__dirname, pathToFixtures, 'file1.json');
-  const pathToFile2 = path.join(__dirname, pathToFixtures, 'file2.json');
-  expect(() => gendiff(pathToFile1, pathToFile2)).toThrow('invalid JSON file content');
-});
-
-describe('empty object', () => {
-  test.each`
-    testPackNumber |  outputFormat
-        ${5}       |  ${defaultOption}
-        ${6}       |  ${plain}
-        ${7}       |  ${json}
-  `('test $fileExtension file extension / empty object / $outputFormat output format',
-  ({ testPackNumber, outputFormat }) => {
-    const pathToFixtures = `/__fixtures__/testPack${testPackNumber}/`;
-    const pathToFile1 = path.join(__dirname, pathToFixtures, 'file1.json');
-    const pathToFile2 = path.join(__dirname, pathToFixtures, 'file2.json');
-    const outputFileContent = readFileSync(
-      path.join(__dirname, pathToFixtures, 'output.txt'),
-      'utf8',
-    );
-    expect(gendiff(pathToFile1, pathToFile2, outputFormat)).toBe(outputFileContent);
+    const received = gendiff(pathToFile1, pathToFile2, outputFormat);
+    const expected = output;
+    expect(received).toBe(expected);
   });
-});
