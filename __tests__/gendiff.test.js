0d86879a58e448214dcbe34041256401e9dc8d76
grom194 <brskoshelev@gmail.com>
2019-08-02 11:47:14 +0300

    final_task: add getFile func

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,9 +5,8 @@ import gendiff from '../src';
 /**
  * Описание
  *
- * fileExtension1, fileExtension2  // расширения сравниваемых файлов
+ * fileExtension  // расширение сравниваемых файлов
  * outputFormat // выбранный тип форматирования результата выполнения
- * fixtureFolder // название папки с тестовыми данными
  *
  * pathToFixtures // путь к тестовым данным текущего теста
  * pathToFile1 // путь к первому сравниваемому файлу
@@ -16,25 +15,48 @@ import gendiff from '../src';
  *
  */
 
+const genGetPathForFunc = pathToFixtures => (file) => {
+  const [name, ext] = file.split('.');
+  switch (name) {
+    case 'file1':
+    case 'file2':
+      return path.join(__dirname, pathToFixtures, 'inputData', ext, `${name}.${ext}`);
+    case 'defaultOption':
+    case 'plain':
+    case 'json':
+      return path.join(__dirname, pathToFixtures, 'outputData', `${name}.${ext}`);
+    default:
+      throw new Error('incorrect file');
+  }
+};
+
 describe('diff nested structures', () => {
-  test.each([
-    ['json', 'json', 'defaultOption', 'nested_structure'],
-    ['ini', 'ini', 'plain', 'nested_structure'],
-    ['yaml', 'yaml', 'json', 'nested_structure'],
-  ])('%#.  gendiff file1.%s file2.%s -f %s ',
-    (file1Extension, file2Extension, outputFormat, fixtureFolder) => {
-      const pathToFixtures = `/__fixtures__/${fixtureFolder}/`;
-      const pathToFile1 = path.join(__dirname, pathToFixtures, 'inputData', file1Extension, `file1.${file1Extension}`);
-      const pathToFile2 = path.join(__dirname, pathToFixtures, 'inputData', file2Extension, `file2.${file2Extension}`);
-      const pathToOutputFile = path.join(__dirname, pathToFixtures, 'outputData', `${outputFormat}.txt`);
+  test.each`
+   fileExtension |  outputFormat
+    ${'json'}    | ${'defaultOption'}
+    ${'ini'}     |    ${'plain'}
+    ${'yaml'}    |    ${'json'}
+`('gendiff file1.$fileExtension file2.$fileExtension -f $outputFormat ',
+  ({ fileExtension, outputFormat }) => {
+    const pathToFixtures = '/__fixtures__/nested_structure/';
+    const getPathFor = genGetPathForFunc(pathToFixtures);
+    const [
+      pathToFile1,
+      pathToFile2,
+      pathToOutputFile,
+    ] = [
+      getPathFor(`file1.${fileExtension}`),
+      getPathFor(`file2.${fileExtension}`),
+      getPathFor(`${outputFormat}.txt`),
+    ];
 
-      const output = readFileSync(
-        pathToOutputFile,
-        'utf8',
-      );
+    const output = readFileSync(
+      pathToOutputFile,
+      'utf8',
+    );
 
-      const received = gendiff(pathToFile1, pathToFile2, outputFormat);
-      const expected = output;
-      expect(received).toBe(expected);
-    });
+    const received = gendiff(pathToFile1, pathToFile2, outputFormat);
+    const expected = output;
+    expect(received).toBe(expected);
+  });
 });
