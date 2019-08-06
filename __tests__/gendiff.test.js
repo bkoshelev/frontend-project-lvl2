bed72a1123d6b0744a20407a365b2059bf303391
grom194 <brskoshelev@gmail.com>
2019-08-06 16:28:09 +0300

    final_task: refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,29 +2,16 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import gendiff from '../src';
 
-/**
- * Описание
- *
- * fileExtension  // расширение сравниваемых файлов
- * outputFormat // выбранный тип форматирования результата выполнения
- *
- * pathToFixtures // путь к тестовым данным текущего теста
- * pathToFile1 // путь к первому сравниваемому файлу
- * pathToFile2 // путь ко второму сравниваемому файлу
- * pathToOutputFile // путь к файлу с резултатом выполнения пакета
- *
- */
-
-const genGetPathForFunc = pathToFixtures => (file) => {
+const getPathFor = (file) => {
   const [name, ext] = file.split('.');
   switch (name) {
     case 'file1':
     case 'file2':
-      return path.join(__dirname, pathToFixtures, 'inputData', ext, `${name}.${ext}`);
+      return path.join(__dirname, '/__fixtures__/', 'inputData', ext, `${name}.${ext}`);
     case 'defaultOption':
     case 'plain':
     case 'json':
-      return path.join(__dirname, pathToFixtures, 'outputData', `${name}.${ext}`);
+      return path.join(__dirname, '/__fixtures__/', 'outputData', `${name}.${ext}`);
     default:
       throw new Error('incorrect file');
   }
@@ -38,17 +25,9 @@ describe('diff nested structures', () => {
     ${'yaml'}    |    ${'json'}
 `('gendiff file1.$fileExtension file2.$fileExtension -f $outputFormat ',
   ({ fileExtension, outputFormat }) => {
-    const pathToFixtures = '/__fixtures__/nested_structure/';
-    const getPathFor = genGetPathForFunc(pathToFixtures);
-    const [
-      pathToFile1,
-      pathToFile2,
-      pathToOutputFile,
-    ] = [
-      getPathFor(`file1.${fileExtension}`),
-      getPathFor(`file2.${fileExtension}`),
-      getPathFor(`${outputFormat}.txt`),
-    ];
+    const pathToFile1 = getPathFor(`file1.${fileExtension}`);
+    const pathToFile2 = getPathFor(`file2.${fileExtension}`);
+    const pathToOutputFile = getPathFor(`${outputFormat}.txt`);
 
     const output = readFileSync(
       pathToOutputFile,
