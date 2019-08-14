90bb4dcad3f04057551c2ec2ee84acbf2f2cd0ba
grom194 <brskoshelev@gmail.com>
2019-08-14 20:26:37 +0300

    final_task: little fixes

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,7 @@ import generateJsonFormatOutput from './json';
 import generatePlainFormatOutput from './plain';
 import generatePrettyFormatOutput from './pretty';
 
-const getFormatter = (diffTree, format = 'defaultOption') => {
+const getFormatter = (diffTree, format = 'pretty') => {
   const formatters = {
     pretty: generatePrettyFormatOutput,
     plain: generatePlainFormatOutput,
--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 import { parse as parseIni } from 'ini';
 
-const parseFileContent = (fileExt, parseContent) => {
+const parseFileContent = (fileExt, parsingContent) => {
   const parsers = {
     '.json': JSON.parse,
     '.yaml': yaml.safeLoad,
     '.ini': parseIni,
   };
 
-  return parsers[fileExt](parseContent);
+  return parsers[fileExt](parsingContent);
 };
 
 export default parseFileContent;
