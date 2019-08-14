b1647b84a0721707d7dce103bc0fb31679f625f7
grom194 <brskoshelev@gmail.com>
2019-08-14 20:26:25 +0300

    final_task: update json format

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,27 +1,31 @@
-import { flattenDeep, isObject, isString } from 'lodash/fp';
+import { flattenDeep } from 'lodash/fp';
 
 const genPath = (path, key) => `${path}${path.length === 0 ? '' : '.'}${key}`;
 
-const formatValue = (value) => {
-  if (isObject(value)) return '[complex value]';
-  if (isString(value)) return `'${value}'`;
-  return value;
-};
-
 const generateJsonFormatOutputText = (diffTree) => {
-  const genJson = key => details => ({ key, details });
-
   const formatNodes = (nodes, pathToNodes = '') => {
     const formatNode = (node, pathToNode) => {
-      const genJsonWithKey = genJson(pathToNode);
-
       const types = {
+        nested: () => formatNodes(node.children, pathToNode),
         unchanged: () => [],
-        added: ({ value2 }) => genJsonWithKey(`Property '${pathToNode}' was added with value: ${formatValue(value2)}`),
-        removed: () => genJsonWithKey(`Property '${pathToNode}' was removed`),
-        changed: ({ value1, value2, children }) => (children.length > 0
-          ? formatNodes(node.children, pathToNode)
-          : genJsonWithKey(`Property '${pathToNode}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`)),
+        added: ({ meta: { value2 } }) => ({
+          event: 'added',
+          oldValue: null,
+          newValue: value2,
+          path: pathToNode,
+        }),
+        removed: ({ meta: { value1 } }) => ({
+          event: 'removed',
+          oldValue: value1,
+          newValue: null,
+          path: pathToNode,
+        }),
+        changed: ({ meta: { value1, value2 } }) => ({
+          event: 'changed',
+          oldValue: value1,
+          newValue: value2,
+          path: pathToNode,
+        }),
       };
       return types[node.nodeType](node);
     };
@@ -29,7 +33,7 @@ const generateJsonFormatOutputText = (diffTree) => {
     return nodes.map((node) => {
       const pathToNode = genPath(
         pathToNodes,
-        node.propKey,
+        node.meta.propKey,
       );
       return formatNode(node, pathToNode);
     });
@@ -42,6 +46,7 @@ const generateJsonFormatOutputText = (diffTree) => {
   const outputText = {
     diffs,
   };
+
 
   return JSON.stringify(outputText, undefined, 2);
 };
